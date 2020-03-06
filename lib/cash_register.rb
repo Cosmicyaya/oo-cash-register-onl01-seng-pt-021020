bcbc2b506749fe9615de338878d312748c0299dd
Ryan Hollins <ballbaroi@gmail.com>
2020-03-06 16:01:51 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -23,6 +23,7 @@ def apply_discount
     @total = @total * (1 - @employee_discount / 100)
     "After the discount the total comes to #{@total}"
   else
-    "There is no discount to apply"
+    "There is no discount to apply."
   end
- end
+end
+end
