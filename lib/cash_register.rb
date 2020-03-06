e58c7c35cd86a4b7589bc80ba9b68c2563603c5a
Ryan Hollins <ballbaroi@gmail.com>
2020-03-06 15:39:40 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,14 @@
-
+class CashRegister
+  attr_accessor :toatal, :discount, :items
+  
+  def initialize (discount = nil)
+    @total = 0
+    @discount = discount
+    @items = []
+  end
+  def.discount
+  self.employee_discount
+end
+def items
+  @items
+end
