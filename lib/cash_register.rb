04929cd9875c3a57b48e05cd9f7f890ab444f14b
Ryan Hollins <ballbaroi@gmail.com>
2020-03-06 16:03:08 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,7 +1,7 @@
 class CashRegister
   attr_accessor :total, :employee_discount, :items
   
-  def initialize (discount = nil)
+  def initialize (employee_discount = nil)
     @total = 0
     @employee_discount = discount
     @items = []
