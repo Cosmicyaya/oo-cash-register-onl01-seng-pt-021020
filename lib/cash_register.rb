5c0ea07f63c2a988b99b2c986d5851b3208d993c
Ryan Hollins <ballbaroi@gmail.com>
2020-03-06 16:39:38 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -23,7 +23,7 @@ end
 
 def apply_discount
   if @discount > 0
-    @discount = @disoucnt/100.to_f
+    @discount = @disount/100.to_f
     @total = @total - (@total * (@discount))
     "After the discount, the total comes to $#{@total.to_i}."
   else
